84c31cc5c289dc8ff20226cfee3e3919bac46a9a
Bruno Azevedo <brunogmn751@gmail.com>
2026-01-17 13:39:59 -0300

    updated the code to work with the browser's local storage, so taks doesn't disappear when refreshing. Also changed the CSS so the task input field is bigger

--- script.js
+++ script.js
@@ -6,25 +6,51 @@ const incompleteTracker = document.querySelector("#incomplete");
 
 let done = 0;
 let incomplete = 0;
+let tasksArray = [];
 
 function updateTasks() {
   doneTracker.innerText = "Done: " + done;
   incompleteTracker.innerText = "Incomplete: " + incomplete;
 }
 
-function addTask() {
+function createTask() {
   if (taskInput.value === "") {
     return;
+  }
+
+  let task = {
+    name: taskInput.value,
+    done: false,
+    id: Math.round(Math.random() * 100000000),
   };
 
+  tasksArray.push(task);
+  saveTasks();
+  renderTask(tasksArray[tasksArray.length - 1]);
+}
+
+function saveTasks() {
+  let ArrayToString = JSON.stringify(tasksArray);
+  localStorage.setItem("savedTasks", ArrayToString);
+}
+
+function renderTask(task) {
   const newLi = document.createElement("li");
   newLi.classList.add("list-item");
+  newLi.id = JSON.stringify(task.id);
   const newDiv = document.createElement("div");
   const checkInput = document.createElement("input");
   checkInput.type = "checkbox";
   checkInput.onchange = doneTask;
   const pElement = document.createElement("p");
-  pElement.innerText = taskInput.value;
+  pElement.innerText = task.name;
+  if (task.done === true) {
+    checkInput.checked = "checked";
+    pElement.classList.add("done-text");
+    done++;
+  } else {
+    incomplete++;
+  }
   const closeIcon = document.createElement("img");
   closeIcon.src = "./images/close-icon.webp";
   closeIcon.onclick = closeTask;
@@ -36,40 +62,70 @@ function addTask() {
 
   taskList.appendChild(newLi);
   taskInput.value = "";
-  incomplete++;
   updateTasks();
 }
 
-function closeTask() {
-  if (this.parentNode.classList.contains("done-task")) {
-    done--;
-  } else {
-    incomplete--;
+addButton.addEventListener("click", createTask);
+taskInput.addEventListener("keydown", function add(event) {
+  if (event.key === "Enter") {
+    createTask();
   }
+});
+
+function closeTask() {
+  let closingTaskId = Number(this.parentNode.id);
+  for (let i = 0; i < tasksArray.length; i++) {
+    if (tasksArray[i].id === closingTaskId) {
+      if (tasksArray[i].done === true) {
+        done--;
+      } else {
+        incomplete--;
+      }
+      tasksArray.splice(i, 1);
+      break;
+    }
+  }
+  saveTasks();
   taskList.removeChild(this.parentNode);
   updateTasks();
 }
 
 function doneTask() {
-  console.log("rodou");
-  if (this.checked) {
-    this.parentNode.parentNode.classList.add("done-task");
-    this.nextElementSibling.classList.add("done-text");
-    done++;
-    incomplete--;
-    updateTasks();
-  } else {
-    this.parentNode.parentNode.classList.remove("done-task");
-    this.nextElementSibling.classList.remove("done-text");
-    done--;
-    incomplete++;
-    updateTasks();
+  let doneTaskId = Number(this.parentNode.parentNode.id);
+  for (let i = 0; i < tasksArray.length; i++) {
+    if (tasksArray[i].id === doneTaskId) {
+      if (this.checked) {
+        tasksArray[i].done = true;
+        this.nextElementSibling.classList.add("done-text");
+        done++;
+        incomplete--;
+        saveTasks();
+        updateTasks();
+      } else {
+        tasksArray[i].done = false;
+        this.nextElementSibling.classList.remove("done-text");
+        done--;
+        incomplete++;
+        saveTasks();
+        updateTasks();
+      }
+    }
   }
 }
 
-addButton.addEventListener("click", addTask);
-taskInput.addEventListener("keydown", function add(event) {
-  if (event.key === "Enter") {
-    addTask();
+function loadTasks() {
+  let recoveredData = localStorage.getItem("savedTasks");
+  let recoveredArray = JSON.parse(recoveredData);
+
+  if (recoveredArray.length === 0) {
+    return;
   }
-});
+
+  for (let i = 0; i < recoveredArray.length; i++) {
+    renderTask(recoveredArray[i]);
+  }
+
+  tasksArray = [...recoveredArray];
+}
+loadTasks();
+
